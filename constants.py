60e1dd125405ab6a52214b990268473aeac171ba
rebecaalbuquerque <rebeca.albuquerque96@gmail.com>
2020-10-12 17:29:46 -0300

    updating paths

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
-PATH_SERIES_TEMPORAIS_SINTETICAS = "static/gerador_ts/tsSinteticas"
-PATH_METRICAS = "static/gerador_ts/metricas"
+from os.path import join, dirname, realpath
+
+PATH_SERIES_TEMPORAIS_SINTETICAS = join(dirname(realpath(__file__)), "static/gerador_ts/tsSinteticas")
+PATH_METRICAS = join(dirname(realpath(__file__)), "static/gerador_ts/metricas")
